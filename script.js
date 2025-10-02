ca474e287f0ddd9199d4784092a23af0427876b2
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-10-02 22:16:37 +0900

    設定ファイルを追加し、スクリプトにクラスの定義を追加

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 人間生成するためのクラス
 class Human {
   happy = 1000;
   happyTurnPowder = 100;
@@ -16,3 +17,20 @@ const ichikadon = new Human("いちかどん");
 
 console.log(happyIchika);
 console.log(ichikadon);
+
+// お家生成のためのクラス
+class House {
+  furnishings = ["PC", "Laundry"];
+  meals = ["さつまいも", "キャラメルポップコーン", "いちかおうどん"];
+  houseplants = "cactus";
+
+  constructor(houseName, locationName) {
+    this.name = houseName;
+    this.location = locationName;
+  }
+}
+const ichikaHouse = new House("いちかのハッピーなお家", "さつまいもの里1番地");
+const ichikadonHouse = new House("いちかどんのお家", "親子丼の里1番地");
+
+console.log(ichikaHouse);
+console.log(ichikadonHouse);
