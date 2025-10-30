fcbd82234f3cce22742776e9a5a45b4a1b8f4387
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-10-30 21:48:28 +0900

    feat: settings.jsonに新しい単語「tokei」を追加し、script.jsにfind()メソッドの練習コードを追加

--- 11-array-higherOrderFunc/script.js
+++ 11-array-higherOrderFunc/script.js
@@ -2,3 +2,13 @@ const numbers = [1, 2, 3, 4, 5];
 numbers.forEach((number, index, array) => {
   console.log(`インデックス：${index},値：${number},配列全体：${array}`);
 });
+
+/**
+ * find()メソッドを練習する
+ * アロー関数バージョンと通常の関数を使用してみる✨️
+ */
+
+const found = numbers.find((number) => number > 3);
+console.log("foundの結果：", found);
+
+// 通常の関数バージョン
